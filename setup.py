850ee5655a845cac7c4245de251f2f7b02eca4de
Sara Ghasemi <ghasemi.sarah@gmail.com>
2021-03-15 20:35:55 +0100

    adding the skeleton of the repo

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='an AI solution to reduce industrial downtime by diagnosing the failure of machines using their acoustic footprint',
+    author='Wrijupan Bhattacharyya Sara Ghasemi Niklas Hübel',
+    license='MIT',
+)
