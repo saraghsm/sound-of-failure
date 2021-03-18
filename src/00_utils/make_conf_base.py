9c11bff39a6e8f58da21a6e8bf1f1bad4dff4dbd
wrijupan <wrijupan.bhattacharyya@desy.de>
2021-03-18 20:30:09 +0100

    Add updated scripts for creating config file

---
+++ src/00_utils/make_conf_base.py
@@ -0,0 +1,29 @@
+import os
+from configparser import ConfigParser
+import argparse
+
+# Config parser
+config_base = ConfigParser()
+
+# Argument parser
+parser = argparse.ArgumentParser(
+    description="Params for Spectrogram and Conv AE")
+
+# Add argument through the command line
+parser.add_argument('-raw', '--raw_data', metavar='',
+                    required=True, help='Path to raw data inside gdrive/MyDrive')
+parser.add_argument('-prj', '--project', default='sound-of-failure',
+                    help='directory name of the project')
+args = parser.parse_args()
+
+# Build the directory configuration
+config_base['directories'] = {
+    'raw_data_dir': args.raw_data,
+    'base_dir': os.path.join('/gdrive', 'MyDrive', args.project)
+}
+
+if __name__ == '__main__':
+    config_filename = '/gdrive/MyDrive/sound-of-failure/conf/conf_base.ini'
+
+    with open(config_filename, 'w') as base_file:
+        config_base.write(base_file)
