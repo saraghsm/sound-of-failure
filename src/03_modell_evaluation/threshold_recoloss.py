12d55206ad43c4fae649b90aa8e853699951bfdd
wrijupan <wrijupan.bhattacharyya@desy.de>
2021-03-31 20:25:21 +0200

    Added median+iqr measure, get threshold by optimizing fbeta_score, function to set threshold based on previously known value

--- src/03_modell_evaluation/threshold_recoloss.py
+++ src/03_modell_evaluation/threshold_recoloss.py
@@ -2,35 +2,35 @@ import numpy as np
 ##########################################################
 # Import sklearn libraries
 ##########################################################
-from sklearn.metrics import precision_score
-from sklearn.metrics import recall_score
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_recall_fscore_support
 ##########################################################
 # Import pandas libraries
 ##########################################################
 import pandas as pd
 
+
 ##########################################################
 # Threshold definitions
 ##########################################################
-def threshold_stddev(validation_loss, no_of_std):
+def threshold_median_iqr(validation_loss, times_iqr):
     """
-    Calculates loss threshold from mean and std.
-    deviation of the validation reconstruction loss.
+    Calculates loss threshold from median and interquartile
+    range (IQR) of the validation reconstruction loss.
+    Threshold = median + (times_iqr * IQR)
     Returns the predicted test labels after thresholding.
 
     validation_loss (array): reconstruction loss on the
                               validation data
-    test_loss (array): reconstruction loss on the
-                              test data
 
-    no_of_std (float): Number of std dev away from mean
-                        that should be used as threshold.
+    times_iqr (float): Number to multiply to the IQR for
+                        setting the threshold.
     """
-    loss_mean = np.median(validation_loss)
-    loss_stddev = np.std(validation_loss)
+    loss_median = np.median(validation_loss)
+    loss_iqr = np.subtract(*np.percentile(validation_loss,
+                                          [75, 25]
+                                          ))
 
-    thres = loss_mean + (no_of_std * loss_stddev)
+    thres = loss_median + (times_iqr * loss_iqr)
     return thres
 
 
@@ -50,6 +50,19 @@ def threshold_percentile(validation_loss, percentile):
 
 
 ##########################################################
+# Predicted labels for a given threshold
+##########################################################
+def predicted_labels(reco_loss, thres):
+    if (thres < reco_loss.min()) or (thres > reco_loss.max()):
+        print(
+            "Warning: The threshold is out of bounds from the "
+            "range of the reconstruction loss. Are you sure your "
+            "sample consists of only one class (either normal or abnormal)? "
+            "If not, check the threshold calculation again...")
+    return reco_loss > thres
+
+
+##########################################################
 # Precision, Recall and Accuracy
 ##########################################################
 def calculate_metrics(true_test_labels, pred_test_labels):
@@ -61,16 +74,14 @@ def calculate_metrics(true_test_labels, pred_test_labels):
     thresholding on the reconstruction loss should be
     applied before calculating these metrics.
     """
-    precision = precision_score(true_test_labels,
-                                pred_test_labels)
+    weighted_f1 = precision_recall_fscore_support(true_test_labels,
+                                                  pred_test_labels,
+                                                  beta=3,
+                                                  average='binary')
 
-    recall = recall_score(true_test_labels,
-                          pred_test_labels)
+    precision, recall, fbeta_score, _ = weighted_f1
 
-    accuracy = accuracy_score(true_test_labels,
-                              pred_test_labels)
-
-    return precision, recall, accuracy
+    return precision, recall, fbeta_score
 
 
 ##########################################################
@@ -104,80 +115,80 @@ def metrics_by_variable_threshold(validation_loss,
     threshold = []
     precision = []
     recall = []
-    accuracy = []
+    fbeta_score = []
 
     for criterion in criterion_range:
         if measure == 'percentile':
             thres = threshold_percentile(validation_loss, criterion)
-            pred_test_labels = test_loss > thres
-        elif measure == 'stddev':
-            thres = threshold_stddev(validation_loss, criterion)
-            pred_test_labels = test_loss > thres
+            pred_test_labels = predicted_labels(test_loss, thres)
+        elif measure == 'iqr':
+            thres = threshold_median_iqr(validation_loss, criterion)
+            pred_test_labels = predicted_labels(test_loss, thres)
 
         else:
-            print("measure should be either 'percentile' or 'stdddev'. Exiting...")
+            print("measure should be either 'percentile' or 'iqr'. Exiting...")
             break
 
-        prec, rec, acc = calculate_metrics(test_labels, pred_test_labels)
+        prec, rec, f_scr = calculate_metrics(test_labels, pred_test_labels)
 
         threshold.append(thres)
         precision.append(prec)
         recall.append(rec)
-        accuracy.append(acc)
+        fbeta_score.append(f_scr)
+        # accuracy.append(acc)
 
-    metrics_df = pd.DataFrame({'Threshold_' + measure: threshold,
+    metrics_df = pd.DataFrame({'' + measure: criterion_range,
+                               'Threshold': threshold,
                                'Precision': precision,
                                'Recall': recall,
-                               'Accuracy': accuracy})
+                               'Fbeta_score': fbeta_score})
 
     return metrics_df
 
 
-def optimal_threshold(metric, target_metric_val, metric_df):
+def get_threshold_criterion(metric_df, metric_to_optimize):
     """
-    When test data is available, the optimal threshold is
-    basically the value of the reco loss that best separates
-    test normal and test anomalous data.
+    Return the criterion for thresholding (how many percentile
+    or how much fraction of the interquartile range) that
+    maximizes a metric given by 'metric_to_optimize'. Currently,
+    there are 3 options : 'Precision', 'Recall', 'Fbeta_score'
 
-    This function offers two choice of metrics: accuracy and
-    precision-recall.
-    The aim is to achieve EITHER a high test prediction accuracy
-    OR a high recall and precision score.
-
-    :param metric (str): 'prec_recall' or 'accuracy'
-    :param target_metric_val (list): [target_precision, target_recall]
-                                     OR [target_accuracy]
     :param metric_df: A Dataframe containing the metrics for a
                       variable threshold (return from the function
                       'metrics_by_variable_threshold')
 
-    :return: A DataFrame containing all the possible
-              optimal thresholds and a single optimal threshold
-              calculated as the mean of all possible optimal thresholds.
+    :param metric_to_optimize: Name of the metric to maximize
+                                'Precision', 'Recall', 'Fbeta_score'
+                                Recommended: 'Fbeta_score'
+
+    :return: A criterion for thresholding
     """
-    if metric == 'prec_recall':
-        try:
-            assert len(target_metric_val) == 2
-            target_precision, target_recall = target_metric_val
-            mask = (metric_df.Precision >= target_precision) & (
-                metric_df.Recall >= target_recall)
-            optim_thres_df = metric_df.loc[mask]
-            optim_thres = optim_thres_df.iloc[:, 0].mean()
-            return optim_thres_df, optim_thres
+    # The first index that has the maximum value of the 'metric_to_optimize'
+    idx_with_max_metric = metric_df.loc[:, metric_to_optimize].idxmax()
+    max_metric = metric_df.loc[:, metric_to_optimize].max()
+    criterion_name = metric_df.columns[0]
 
-        except AssertionError:
-            print(
-                "'target_metric_val' should be of length 2 including precision and recall")
+    criterion = np.round(
+        metric_df.loc[idx_with_max_metric, criterion_name], 2)
 
-    elif metric == 'accuracy':
-        try:
-            assert len(target_metric_val) == 1
-            target_accuracy = target_metric_val[0]
-            mask = round(metric_df.Accuracy, 2) >= target_accuracy
-            optim_thres_df = metric_df.loc[mask]
-            optim_thres = optim_thres_df.iloc[:, 0].mean()
-            return optim_thres_df, optim_thres
+    print("The threshold that provides the best {} with value "
+          "{} corresponds to {} {}".format(metric_to_optimize,
+                                           max_metric,
+                                           criterion,
+                                           criterion_name))
+    return {criterion_name: criterion}
 
-        except AssertionError:
-            print(
-                "'target_metric_val' should be a list of length 1 containing target accuracy")
+
+def set_threshold(validation_loss, criterion_dict):
+    """
+    Set a threshold based on a given measure and criterion.
+    'measure' can be 'iqr' or 'percentile'.
+    'criterion' is the value of 'iqr' or 'percentile'.
+    """
+    criterion = list(criterion_dict.keys())[0]
+
+    if criterion == 'iqr':
+        thres = threshold_median_iqr(validation_loss, criterion_dict[criterion])
+    elif criterion == 'percentile':
+        thres = threshold_percentile(validation_loss, criterion_dict[criterion])
+    return thres
